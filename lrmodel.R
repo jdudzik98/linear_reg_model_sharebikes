b3ebc60ae0fea49506c0f1a340d8f6fb00656dee
jandudzik <dudzikjanek@gmail.com>
2019-10-26 20:50:11 +0200

    Created simple linear regression model containing one explanatory variable (felt temperature). Dependent variable is number of rented bikes in one hour. Created also R Markdown

--- lrmodel.R
+++ lrmodel.R
@@ -1,1 +1,12 @@
-print("Hello World!")
+x1 <- as.numeric(as.character(london_merged$t2)) #temperature feels
+
+beta0 = rep(1,times=length(x1)) #creating column of 1's 
+
+X=cbind(beta0,x1)
+
+Y = as.numeric(as.character(london_merged$V2[2:length(london_merged$V2)]))
+
+b=solve(t(X)%*%X)%*%t(X)%*%cbind(Y)
+
+print(b)
+
